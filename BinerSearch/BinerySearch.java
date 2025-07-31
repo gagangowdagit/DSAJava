a00deee5e6dd72aa2927cbc8c5cc73194b1cb82f
gagan gowda <gagan720464@gmail.com>
2025-07-31 23:04:21 +0530

    Binery search

---
+++ BinerSearch/BinerySearch.java
@@ -0,0 +1,14 @@
+package javademo;
+import java.util.*;
+public class binsearch {
+    public static void main(String args[]){
+        Scanner sc  = new Scanner(System.in) ;
+        System.out.println("enter the elements");
+        int n=sc.nextInt();
+        int[] arr1 = new int[n];
+        
+    }
+}
+
+
+write with function based saves the time
