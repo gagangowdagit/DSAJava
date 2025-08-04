f9b8cf4dcf3f97ffcad727fc5ec2c300d4227f32
gagan gowda <gagan720464@gmail.com>
2025-08-04 21:15:14 +0530

    testing

--- BubbleSort/Bubsort.java
+++ BubbleSort/Bubsort.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Bubsort {
     public static void Bubsorting(int arr[], int n ){
+        System.out.println("BubbleShort");
         for(int i=0;i<n-1;i++){
           for(int j=0;j<n-1;j++){
             if(arr[j]>arr[j+1]){
